17c6b5951c0edd51a84925da45fb26ed9a71c085
ExuberantOwl <92175531+ExuberantOwl@users.noreply.github.com>
2021-11-27 16:50:22 -0700

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -12,8 +12,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-//static const char col_cyan[]        = "#8c2604";
-static const char col_cyan[]        = "#4f92ff";
+static const char col_cyan[]        = "#8c2604";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
